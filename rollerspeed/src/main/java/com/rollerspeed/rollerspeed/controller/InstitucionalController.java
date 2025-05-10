34517c4540332b97e7b9e41c59c97269d3d4f0b0
estiv <estiven.bernal@est.iudigital.edu.co>
2025-05-10 12:42:42 -0500

    proyecto

---
+++ rollerspeed/src/main/java/com/rollerspeed/rollerspeed/controller/InstitucionalController.java
@@ -0,0 +1,17 @@
+package com.rollerspeed.rollerspeed.controller;
+
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InstitucionalController {
+
+    @GetMapping("/institucional")
+    public String vistaInstitucional() {
+        return "institucional"; // Muestra institucional.html
+    }
+}
+
